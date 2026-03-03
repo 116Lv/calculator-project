5bc87f6fa3f5030f54b5dcaeaf07c08312f20be6
116_Lv <lbw0109@gmail.com>
2026-03-03 16:41:16 +0900

    fix: 잘못된 입력시 강제종료되던 문제 예외처리 추가
    refactor:
    계산기 시작/종료 메시지 반복 제거

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
@@ -10,17 +11,30 @@ public class Calculator {
 
         Scanner scanner = new Scanner(System.in);
 
-        while(true) {
-            System.out.println("=== Java 계산기 ===");
+        System.out.println("=== Java 계산기 ===");
 
-            System.out.print("첫 번째 숫자: ");
-            num1 = scanner.nextInt();
+        while(true) {
+
+            try {
+                System.out.print("첫 번째 숫자: ");
+                num1 = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력해주세요.");
+                scanner.nextLine();
+                continue;
+            }
 
             System.out.print("연산자: ");
             oper = scanner.next();
 
-            System.out.print("두 번째 숫자: ");
-            num2 = scanner.nextInt();
+            try {
+                System.out.print("두 번째 숫자: ");
+                num2 = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력해주세요.");
+                scanner.nextLine();
+                continue;
+            }
 
             switch(oper) {
                 case "+":
@@ -41,10 +55,10 @@ public class Calculator {
                     break;
             }
 
-            /*System.out.println("계산기를 종료합니다.");*/
-
             break;  // 이렇게 break를 넣어놓지 않으면 scanner.close()에 도달할수 없는 에러가 발생해 일단 넣어놓음
         }
+
+        /*System.out.println("계산기를 종료합니다.");*/
 
         scanner.close();
 
