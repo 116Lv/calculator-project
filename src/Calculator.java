9ac161ef7b92b15ca203cfce3c8f1e2353170d39
116_Lv <lbw0109@gmail.com>
2026-03-03 15:58:00 +0900

    feat: 입력받아야할 변수 초기화 Scanner객체 생성 while 무한반복문 생성 숫자 및 연산자 입력기능 구현

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,12 +3,31 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
+        int num1 = 0;
+        int num2 = 0;
+        String oper = "";
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== Java 계산기 ===");
+        while(true) {
+            System.out.println("=== Java 계산기 ===");
 
-        System.out.println("계산기를 종료합니다.");
+            System.out.println("첫번쨰 숫자를 입력하세요: ");
+            num1 = scanner.nextInt();
+
+            System.out.println("연산자를 입력하세요 (+, -, *, /): ");
+            oper = scanner.next();
+
+            System.out.println("두번쨰 숫자를 입력하세요: ");
+            num2 = scanner.nextInt();
+
+            System.out.println("계산기를 종료합니다.");
+
+            break;  // 이렇게 break를 넣어놓지 않으면 scanner.close()에 도달할수 없는 에러가 발생해 일단 넣어놓음
+        }
+
         scanner.close();
+
     }
 
 }
