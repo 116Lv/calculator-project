5d189248e062b00ab5d8fb7a6a42849687fb4aae
116_Lv <lbw0109@gmail.com>
2026-03-03 16:20:39 +0900

    feat: 연산 결과값 변수 추가 입력값 기반 계산 결과 출력 구현
    refactor:
    입력 안내 문구 Step2 예시와 동일하게 수정
    종료 안내문 주석 처리
    불필요한 변수 초기화 제거

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,25 +3,45 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
-        int num1 = 0;
-        int num2 = 0;
-        String oper = "";
+        int num1;
+        int num2;
+        float result;
+        String oper;
 
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
             System.out.println("=== Java 계산기 ===");
 
-            System.out.println("첫번쨰 숫자를 입력하세요: ");
+            System.out.print("첫 번째 숫자: ");
             num1 = scanner.nextInt();
 
-            System.out.println("연산자를 입력하세요 (+, -, *, /): ");
+            System.out.print("연산자: ");
             oper = scanner.next();
 
-            System.out.println("두번쨰 숫자를 입력하세요: ");
+            System.out.print("두 번째 숫자: ");
             num2 = scanner.nextInt();
 
-            System.out.println("계산기를 종료합니다.");
+            switch(oper) {
+                case "+":
+                    result = num1+num2;
+                    System.out.println("결과: " + result);
+                    break;
+                case "-":
+                    result = num1-num2;
+                    System.out.println("결과: " + result);
+                    break;
+                case "*":
+                    result = num1*num2;
+                    System.out.println("결과: " + result);
+                    break;
+                case "/":
+                    result = num1/num2;
+                    System.out.println("결과: " + result);
+                    break;
+            }
+
+            /*System.out.println("계산기를 종료합니다.");*/
 
             break;  // 이렇게 break를 넣어놓지 않으면 scanner.close()에 도달할수 없는 에러가 발생해 일단 넣어놓음
         }
