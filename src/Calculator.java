592e1857933ca38e742e7ed1855dbc2f4de070e7
116_Lv <lbw0109@gmail.com>
2026-03-04 19:02:41 +0900

    feat: 메뉴 추가
    refactor:
    메뉴화면 추가로 인한 기존 실행순서및 방식 변경

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,14 +8,70 @@ public class Calculator {
 
         Calculator calc = new Calculator();
 
-        calc.runCalculator();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean endPoint = false;
+
+        while(!endPoint) {
+            switch(calc.MainPage(scanner)) {
+                case 1:
+                    calc.runCalculator(scanner);
+                    break;
+                case 2:
+                    calc.watchHistory();
+                    break;
+                case 3:
+                    calc.deleteHistory();
+                    break;
+                case 0:
+                    System.out.println("완전히 종료합니다");
+                    endPoint = true;
+                    scanner.close();
+                    break;
+                default:
+                    System.out.println("올바른 메뉴번호를 입력해주세요.");
+            }
+        }
+    }
+
+    //메뉴화면
+    public int MainPage(Scanner scanner) {
+        System.out.println("=== 계산기 메뉴 ===");
+        System.out.println("1. 계산하기");
+        System.out.println("2. 계산 이력 보기");
+        System.out.println("3. 이력 지우기");
+        System.out.println("0. 종료");
+        System.out.print("선택: ");
+
+        return scanner.nextInt();
+    }
+
+    //기록 조회
+    public void watchHistory() {
+        if(storedList.isEmpty()) {
+            System.out.println("\n아직 연산된 이력이 없습니다.\n");
+        } else {
+            System.out.println("\n=== 최근에 연산한 이력 ===");
+            for(int i = storedList.size() - 1; i >= 0; i--) {
+                Record record = storedList.get(i);
+                if(record.oper.equals("sqrt")) {
+                    System.out.println(record.num1 + "의 제곱근 = " + record.result);
+                } else {
+                    System.out.println(record.num1 + " " + record.oper + " " + record.num2 + " = " + record.result);
+                }
+            }
+            System.out.println("총 " + storedList.size() + " 개의 이력이 조회되었습니다.\n처음 화면으로 돌아갑니다.\n");
+        }
+    }
+
+    //기록 삭제
+    public void deleteHistory() {
+        storedList.clear();
+        System.out.println("모든 이력이 삭제되었습니다.\n처음 화면으로 돌아갑니다.");
     }
 
     //계산하기
-    public void runCalculator() {
-
-
-        Scanner scanner = new Scanner(System.in);
+    public void runCalculator(Scanner scanner) {
 
         while(true) {
             double num1;
@@ -23,7 +79,7 @@ public class Calculator {
             double num2;
             double result;
 
-            System.out.println("=== Java 계산기 ===");
+            System.out.println("\n=== Java 계산기 ===");
             num1 = inputNumber(scanner, "첫", oper);
             oper = inputOperator(scanner, num1);
             if(oper.equals("sqrt")) {
@@ -36,7 +92,6 @@ public class Calculator {
             storedList.add(new Record(num1, oper, num2, result));
 
             if(!askContinue(scanner)) {
-                scanner.close();
                 break;
             }
         }
